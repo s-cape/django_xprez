2702a4270811460106a28e8eef73df8ad293b50e
Martin Kappel <kilpak@gmail.com>
2022-02-09 10:29:51 +0100

    numbers module minwidth fix

--- xprez/static/xprez/js/numbers.js
+++ xprez/static/xprez/js/numbers.js
@@ -1,14 +1,13 @@
 function initNumbers() {
     $('.js-number-container').each(function (index, el) {
         var $el = $(el);
-        $el.css('min-width', ($el.width() + 10).toString() + 'px');
+        var w = $el.outerWidth();
+        $el.css('min-width', (w + 10).toString() + 'px');
     });
     $('.js-counter-up').counterUp({
-        // delay: 30,
         time: 3000
     });
 }
-
 
 $(document).ready(function () {
     initNumbers();
