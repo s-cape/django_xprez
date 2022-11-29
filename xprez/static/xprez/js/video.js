1b9a3a87167c94a4bfdfa718f41ad0aa16c08ccb
Jakub Dolejsek <jakub.dolejsek@gmail.com>
2022-11-29 15:00:41 +0100

    video posters click-through, new vimeo api

--- xprez/static/xprez/js/video.js
+++ xprez/static/xprez/js/video.js
@@ -1,40 +1,48 @@
-function handlePoster($contents) {
-    $contents.each(function (index, el) {
-        var $el = $(el);
-        var $poster = $el.find('.js-poster');
-        $poster.on('click', function () {
-            var $youtube = $el.find('.js-youtube');
-            if ($youtube.length) {
-                new YT.Player($youtube.attr('id'), {
-                  events: {
-                    'onReady': function(event) {
-                        event.target.playVideo();
-                    }
-                  }
-                });
-                setTimeout(function() {
-                    $poster.hide();
-                }, 250);
-
-            }
-            var $vimeo = $el.find('.js-vimeo');
-            if ($vimeo.length) {
-                var player = $f($vimeo[0]);
-                player.api("play");
+function xprezInitVimeo() {
+    $('.js-video-with-poster').each(function (index, el) {
+        var $wrapper = $(el);
+        var $iframe = $wrapper.find('.js-vimeo');
+        if ($iframe.length) {
+            var $poster = $wrapper.find('.js-poster');
+            var player = new Vimeo.Player($iframe[0]);
+            player.on('play', function() {
                 $poster.hide();
-
-            }
-        })
-
+            });
+        }
     });
-
 }
 
-function initPosters() {
-    handlePoster($('.js-video-with-poster'));
+function xprezInitYoutube() {
+    $('.js-video-with-poster').each(function (index, el) {
+        var $wrapper = $(el);
+        var $iframe = $wrapper.find('.js-youtube');
+        if ($iframe.length) {
+            var $poster = $wrapper.find('.js-poster');
+            new YT.Player($iframe.attr('id'), {
+                events: {
+                    'onStateChange': function(event) {
+                        if (event.data == YT.PlayerState.PLAYING) {
+                            $poster.hide();
+                        }
+                    }
+                }
+            });
+        }
+    });
 }
 
+function xprezInitVideoFallback() {
+    // just in case pointer-events: none does not work (opera mini)
+    $('.js-video-with-poster .js-poster').click(function () {
+        $(this).hide();
+    });
+}
 
 $(function () {
-    initPosters()
+    xprezInitVimeo();
+    // youtube init is handled by the youtube onYoutubeIframeAPIReady callback
+    xprezInitVideoFallback();
 });
+
+function onYouTubeIframeAPIReady() { xprezInitYoutube; }
+window.onYouTubeIframeAPIReady = onYouTubeIframeAPIReady;
