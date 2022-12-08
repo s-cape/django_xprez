724f29d424ac1165816f432cbd1ab4ccc9175c34
Jakub Dolejsek <jakub.dolejsek@gmail.com>
2022-12-08 15:02:23 +0100

    ckeditor parse text - fix for inline images

--- xprez/ck_editor/parse_text.py
+++ xprez/ck_editor/parse_text.py
@@ -12,6 +12,58 @@ except ImportError:
     import io
 
 
+def _replace_wrapper_with_templatetag(wrapper, img, request):
+    # wrapper and img may be the same tag - that is the case of inline images
+    src = img.get("src")
+
+    if not src.lower().startswith("http") and request:
+        src = request.build_absolute_uri(src)
+
+    if sys.version_info >= (3, 0):
+        file = io.BytesIO(urllib.request.urlopen(src).read())
+    else:
+        file = cStringIO.StringIO(urllib.urlopen(src).read())
+    im = Image.open(file)
+    width, height = im.size
+    classes = list(img.get("class", [])) + list(wrapper.get("class", []))
+    if "image-style-align-right" in classes:
+        align = "right"
+    elif "image-style-align-left" in classes:
+        align = "left"
+    else:
+        align = "center"
+
+    caption_tag = wrapper.find("figcaption")
+    if caption_tag:
+        caption = caption_tag.text
+    else:
+        caption = ""
+
+    alt_text = img.get("alt", "")
+
+    tag_link = wrapper.find("a")
+    if tag_link:
+        link_url = tag_link.get("href")
+        link_new_window = tag_link.get("target") == "_blank"
+    else:
+        link_url = ""
+        link_new_window = False
+
+    wrapper.replaceWith(
+        '{%% ckeditor_content_image "%s" "%s" %s %s caption="%s" alt_text="%s" link_url="%s" link_new_window=%s %%}'
+        % (
+            src,
+            align,
+            width,
+            height,
+            caption,
+            alt_text,
+            link_url,
+            "True" if link_new_window else "False",
+        )
+    )
+
+
 def parse_text(text_source, request):
     if not text_source:
         return ""
@@ -24,57 +76,16 @@ def parse_text(text_source, request):
         else:
             last_p.extract()
 
-    for tag in soup.find_all("figure", attrs={"class": "image"}):
+    for wrapper in soup.find_all("figure", attrs={"class": "image"}):
+        img = wrapper.find("img")
+        if img:
+            _replace_wrapper_with_templatetag(wrapper, img, request)
 
-        tag_img = tag.find("img")
-        if tag_img:
-            url = tag_img.get("src")
-
-            if not url.lower().startswith("http") and request:
-                url = request.build_absolute_uri(url)
-
-            if sys.version_info >= (3, 0):
-                file = io.BytesIO(urllib.request.urlopen(url).read())
-            else:
-                file = cStringIO.StringIO(urllib.urlopen(url).read())
-            im = Image.open(file)
-            width, height = im.size
-            if "image-style-align-right" in tag["class"]:
-                align = "right"
-            elif "image-style-align-left" in tag["class"]:
-                align = "left"
-            else:
-                align = "center"
-
-            caption_tag = tag.find("figcaption")
-            if caption_tag:
-                caption = caption_tag.text
-            else:
-                caption = ""
-
-            alt_text = tag_img.get("alt", "")
-
-            tag_link = tag.find("a")
-            if tag_link:
-                link_url = tag_link.get("href")
-                link_new_window = tag_link.get("target") == "_blank"
-            else:
-                link_url = ""
-                link_new_window = False
-
-            tag.replaceWith(
-                '{%% ckeditor_content_image "%s" "%s" %s %s caption="%s" alt_text="%s" link_url="%s" link_new_window=%s %%}'
-                % (
-                    url,
-                    align,
-                    width,
-                    height,
-                    caption,
-                    alt_text,
-                    link_url,
-                    "True" if link_new_window else "False",
-                )
-            )
+    inline_images = []
+    inline_images += soup.find_all("img", attrs={"class": "image-style-align-left"})
+    inline_images += soup.find_all("img", attrs={"class": "image-style-align-right"})
+    for img in inline_images:
+        _replace_wrapper_with_templatetag(img, img, request)
 
     for tag in soup.find_all(attrs={"contenteditable": True}):
         del tag["contenteditable"]
