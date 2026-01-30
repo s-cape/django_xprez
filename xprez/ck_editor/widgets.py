d1ec6cfaf6f7c02706ba727691c4065c3df7dcf0
Martin Kappel <martin@mimatik.com>
2026-01-30 15:56:02 +0100

    FE module attributes, add h4 to ckeditor full config

--- xprez/ck_editor/widgets.py
+++ xprez/ck_editor/widgets.py
@@ -103,6 +103,12 @@ class CkEditorWidgetFullBase(CkEditorWidgetBase):
                         "title": "Heading 3",
                         "class": "ck-heading_heading3",
                     },
+                    {
+                        "model": "heading4",
+                        "view": "h4",
+                        "title": "Heading 4",
+                        "class": "ck-heading_heading4",
+                    },
                 ),
             },
             "fontSize": {
