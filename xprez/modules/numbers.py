cbcc15ff3b6a13ec311dbdab7243142e84ba67df
Martin Kappel <martin@mimatik.com>
2026-04-10 17:09:08 +0200

    +

--- xprez/modules/numbers.py
+++ xprez/modules/numbers.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.db import models
-from django.utils.translation import gettext_lazy as _, ngettext
+from django.utils.translation import gettext_lazy as _
+from django.utils.translation import ngettext
 
 from xprez import constants
 from xprez.admin.forms import ModuleForm
